762805c71b7e4d8a4d81305d082662f7bea39ca4
Nic <nic@nicsknickknackshack.com>
2018-11-28 20:53:36 -0500

    Finish insertion cases

--- RedBlackTree.js
+++ RedBlackTree.js
@@ -48,6 +48,16 @@ class Node{
         }
         else return false;
     }
+    getUncle(){
+        if(this.parent !== null && this.parent.parent != null){
+            if(this.parent.isLST()){
+                return this.parent.parent.RST;
+            }
+            else{
+                return this.parent.parent.LST;
+            }
+        }
+    }
 
     refreshColor(){
         if(this.isRed){
@@ -107,6 +117,7 @@ class BinaryTree{
             node.isRed = true;
             this.InsertNodeHelper(this.root, node);
         }
+        return node;
     }
 
     FindMinNode(root){
@@ -194,5 +205,221 @@ class BinaryTree{
 }
 
 class RedBlackTree extends BinaryTree{
+    LeftLeftRotation(node){
+        var uncle = node.getUncle();
+        var parent = node.parent;
+        var grandparent = node.parent.parent;
 
+        var t1 = node.LST;
+        var t2 = node.RST;
+        var t3 = node.parent.RST;
+        var t4;
+        var t5;
+        if(uncle !== null){
+            t4 = uncle.LST;
+            t5 = uncle.RST;
+        }
+        else{
+            t4 = null;
+            t5 = null;
+        }
+
+        if(grandparent.parent != null){
+            if(grandparent.isLST()){
+                grandparent.parent.LST = parent;
+            }
+            else{
+                grandparent.parent.RST = parent;
+            }
+        }
+        else{
+            this.root = parent;
+        }
+
+        parent.parent = grandparent.parent;
+        parent.RST = grandparent;
+        parent.isRed = false;
+        parent.refreshColor();
+
+
+        grandparent.parent = parent;
+        grandparent.LST = t3;
+        grandparent.RST = uncle;
+        grandparent.isRed = true;
+        grandparent.refreshColor();
+
+        if(uncle != null){
+            uncle.parent = grandparent;
+            uncle.LST = t3;
+            uncle.RST = t4;
+            uncle.isRed = false;
+        }
+        
+        this.UpdateTreePositions(this.root);
+        
+        //console.log(this);
+    }
+    LeftRightRotation(node){
+        var parent = node.parent;
+        var grandparent = node.parent.parent;
+
+        var t1 = node.parent.LST;
+        var t2 = node.LST;
+        var t3 = node.RST;
+        
+        grandparent.LST = node;
+        node.parent = grandparent;
+        node.LST = parent;
+        node.RST = t3;
+        parent.parent = node;
+        parent.LST = t1;
+        parent.RST = t2;
+
+        this.UpdateTreePositions(this.root);
+        
+        //console.log(this);
+
+        //Left Left rotatre from parent
+        this.LeftLeftRotation(parent);
+
+    }
+    RightRightRotation(node){
+        var uncle = node.getUncle();
+        var parent = node.parent;
+        var grandparent = node.parent.parent;
+
+        var t1;
+        var t2;
+        if(uncle !== null){
+            t1 = uncle.LST;
+            t2 = uncle.RST;
+        }
+        else{
+            t1 = null;
+            t2 = null;
+        }
+        var t3 = parent.LST;
+        var t4 = node.LST;
+        var t5 = node.RST;
+        
+        
+
+        if(grandparent.parent != null){
+            if(grandparent.isLST()){
+                grandparent.parent.LST = parent;
+            }
+            else{
+                grandparent.parent.RST = parent;
+            }
+        }
+        else{
+            this.root = parent;
+        }
+
+        parent.parent = grandparent.parent;
+        parent.LST = grandparent;
+        parent.isRed = false;
+        parent.refreshColor();
+
+
+        grandparent.parent = parent;
+        grandparent.LST = uncle;
+        grandparent.RST = t3;
+        grandparent.isRed = true;
+        grandparent.refreshColor();
+
+        if(uncle != null){
+            uncle.parent = grandparent;
+            uncle.LST = t1;
+            uncle.RST = t2;
+            uncle.isRed = false;
+        }
+        
+        this.UpdateTreePositions(this.root);
+    }
+
+    RightLeftRotation(node){
+        var parent = node.parent;
+        var grandparent = node.parent.parent;
+
+        var t3 = node.LST;
+        var t4 = node.RST;
+        var t5 = parent.RST;
+        
+        grandparent.RST = node;
+        node.parent = grandparent;
+        node.LST = t3;
+        node.RST = parent;
+        parent.parent = node;
+        parent.LST = t4;
+        parent.RST = t5;
+
+        this.UpdateTreePositions(this.root);
+        
+        //console.log(this);
+
+        //Right-Right rotate from parent
+        this.RightRightRotation(parent);
+    }
+
+
+    ValidateInsertion(node){
+        if(node === this.root){ //Color root black
+            node.isRed = false;
+            return;
+        }
+        if(node.parent.isRed == false){ //Everything is fine as is
+            return;
+        }
+
+        //Shit
+        var uncle = node.getUncle();
+        var parent = node.parent;
+        var grandparent = node.parent.parent;
+
+        if(uncle !== null && uncle.isRed == true){ //Red uncle
+            uncle.isRed = false;
+            parent.isRed = false;
+
+            parent.refreshColor()
+            uncle.refreshColor();
+
+            this.ValidateInsertion(grandparent);
+        }
+        else{ //Black uncle
+            
+            //LEFT LEFT CASE
+            if(node.isLST() === true && node.parent.isLST() === true){
+                this.LeftLeftRotation(node);               
+            }
+
+            //LEFT RIGHT CASE
+            if(node.isLST() === false && node.parent.isLST() === true){
+                this.LeftRightRotation(node);
+            }
+
+            //RIGHT RIGHT CASE
+            if(node.isLST() === false && node.parent.isLST() === false){
+                this.RightRightRotation(node);
+            }
+
+            //RIGHT LEFT CASE
+            if(node.isLST() === true && node.parent.isLST() === false){
+                this.RightLeftRotation(node);
+            }
+
+        }
+
+    }
+
+    InsertNode(value){
+        var node = super.InsertNode(value);
+        //console.log(node);
+        this.ValidateInsertion(node);
+        
+    }
+    RemoveNode(value){
+        super.RemoveNode(value);
+        console.log("RBREMOVE");
+    }
 }
